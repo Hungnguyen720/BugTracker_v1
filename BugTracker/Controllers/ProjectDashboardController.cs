32fb64dc7482ebef2ca3fc5cb412de5cfdcdac85
Hung Nguyen <HungNguyen720@gmail.com>
2020-01-21 20:58:03 -0800

    added charts js

--- BugTracker/Controllers/ProjectDashboardController.cs
+++ BugTracker/Controllers/ProjectDashboardController.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BugTracker.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BugTracker.Controllers
 {
     public class ProjectDashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly ProjectSettingsContext _context;
+
+        public ProjectDashboardController(ProjectSettingsContext context)
         {
-            return View();
+            _context = context;
         }
+
+        [HttpGet()]
+        public async Task<IActionResult> Index(int projectid)
+        {
+            var results = await _context.ProjectSettings.ToListAsync();
+
+            return View(results);
+        }
+
     }
 }
